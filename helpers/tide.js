a001e3898fb9bd03710fc319120098e379044c6e
Conor Macken <cwmacken@gmail.com>
2017-02-22 00:18:39 -0800

    beautify

--- helpers/tide.js
+++ helpers/tide.js
@@ -1,4 +1,4 @@
-// Run from root dir with: node ./helpers/tide.js 
+// Run from root dir with: node ./helpers/tide.js
 
 var momentTimezone = require('moment-timezone'),
     moment = require('moment'),
@@ -10,10 +10,10 @@ var tides = require('../lib/tides');
 
 console.log("run tide.js");
 
-tides.getTideLa(function(response){
-  if(response === null){
-    console.log("LA tide graph fetch successful")
-  }else{
-    console.log("ERROR: LA tide graph fetch error", response)
-  }
+tides.getTideLa(function(response) {
+    if (response === null) {
+        console.log("LA tide graph fetch successful")
+    } else {
+        console.log("ERROR: LA tide graph fetch error", response)
+    }
 });
