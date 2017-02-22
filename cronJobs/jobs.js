b6fb321d603b43d85c15023d50cc60771b8b45a2
Conor Macken <cwmacken@gmail.com>
2017-02-22 00:09:49 -0800

    Revert "testing tabs correction"
    This reverts commit d3472b64dbe836bc3cb186b164b08e845bbd2793.

--- cronJobs/jobs.js
+++ cronJobs/jobs.js
@@ -2,17 +2,19 @@ var CronJob = require('cron').CronJob;
 var tides = require('../lib/tides');
 
 var exports = module.exports = {
+
 	setTideCronJobs: function(cb){
 		new CronJob('0 0 0 * * *', function() {
-			console.log('You will see this message at 12 am LA time');
-			tides.getTideLa(function(response){
-				if(response === null){
-					console.log("LA tide graph fetch successful")
-				}else{
-					console.log("ERROR: LA tide graph fetch error", response)
-				}
-			});
+				console.log('You will see this message at 12 am LA time');
+				tides.getTideLa(function(response){
+					if(response === null){
+						console.log("LA tide graph fetch successful")
+					}else{
+						console.log("ERROR: LA tide graph fetch error", response)
+					}
+				});
 		}, null, true, 'America/Los_Angeles');
 		cb();
 	}
+
 }
