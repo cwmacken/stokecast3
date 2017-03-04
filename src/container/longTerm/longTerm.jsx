9edb14dd94d644afcb1fd53b36bfef4269a17cab
Conor Macken <cwmacken@gmail.com>
2017-03-03 17:01:07 -0800

    displaying avg sizes

--- src/container/longTerm/longTerm.jsx
+++ src/container/longTerm/longTerm.jsx
@@ -13,6 +13,7 @@ import Header from '../../components/header/header.jsx'
 import Timeframe from '../../components/timeFrame/timeFrame.jsx'
 import {Link} from 'react-router'
 // require("./location.less")
+// http://recharts.org/#/en-US/
 import {getSwellData} from '../../redux/actions'
 
 const swellModelTitle = (
@@ -48,17 +49,82 @@ class Longterm extends React.Component {
     }
 
     displaySwellData() {
+
+        var dayCheck = null
+
+        var heightArrayAM = []
+
+        var heightArrayPM = []
+
         const swellData = this.props.swell.map((dataPoint, i) => {
 
             var height = dataPoint.hst / 0.3048
 
-            height = Math.floor(height)
+            if (dayCheck === null) {
 
-            return (
-                <h3 key={i}>{dataPoint.date}
-                    - {dataPoint.hour}
-                    - {height}ft</h3>
-            )
+                dayCheck = dataPoint.day
+
+            } else if (dayCheck === dataPoint.day) {
+
+                if (dataPoint.hour === '6AM' || dataPoint.hour === '7AM' || dataPoint.hour === '8AM' || dataPoint.hour === '9AM' || dataPoint.hour === '10AM' || dataPoint.hour === '11AM') {
+
+                    heightArrayAM.push(height)
+                }
+
+                if (dataPoint.hour === '12PM' || dataPoint.hour === '1PM' || dataPoint.hour === '2PM' || dataPoint.hour === '3PM' || dataPoint.hour === '4PM' || dataPoint.hour === '5PM') {
+
+                    heightArrayPM.push(height)
+                }
+
+                if (dataPoint.hour === '12PM') {
+
+                    var htSum = 0
+
+                    for (var k = 0; k < heightArrayAM.length; k++) {
+                        htSum = htSum + heightArrayAM[k]
+                    }
+
+                    var avg = htSum / heightArrayAM.length
+
+                    var displayHeight = Math.floor(avg)
+
+                    var msg = dayCheck + " AM Swell: " + displayHeight + " ft"
+
+                    return (
+                        <h3 key={i}>{msg}</h3>
+                    )
+
+                }
+
+                if (dataPoint.hour === '6PM') {
+
+                    var htSum = 0
+
+                    for (var k = 0; k < heightArrayPM.length; k++) {
+                        htSum = htSum + heightArrayPM[k]
+                    }
+
+                    var avg = htSum / heightArrayPM.length
+
+                    var displayHeight = Math.floor(avg)
+
+                    var msg = dayCheck + " PM Swell: " + displayHeight + " ft"
+
+                    return (
+                        <h3 key={i}>{msg}</h3>
+                    )
+
+                }
+
+            } else {
+
+                heightArrayAM = []
+                heightArrayPM = []
+
+                dayCheck = null
+
+            }
+
         });
         return swellData
     }
